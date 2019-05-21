8437c75666a79ff45e46e08c96408fa57ea8d795
bembenggg <bembenggg@gmail.com>
2019-05-21 12:15:19 +0700

    detailmenu

--- src/containers/screen/DetailMenu.js
+++ src/containers/screen/DetailMenu.js
@@ -3,6 +3,7 @@ import {
     View,
     Text,
     Image,
+    Button,
     ScrollView,
     TouchableOpacity,
     SafeAreaView,
@@ -23,26 +24,62 @@ import { GlobalConsumer } from '../../contexts/Context';
 import ItemsList from '../../components/ItemsList';
 
 
+const styles = AppStyles.login;
 class DetailMenu extends Component {
     constructor(props) {
         super(props)
         const { navigation } = this.props;
+        // console.warn(navigation)
         this.state = {
-            items: []
+            item : navigation.state.params
+            
+            
         }
 
     }
-  render(){
-        return (
-            <View>
-             {
-                this.state.items.length > 0 ?
-                this.state.items.map((value, index) => {
-                return (
-                <ItemsList key={index} data={value} />
-                );
-                    }): <></>
+    handleGetItem = () => {
+        API.getRedeemItems().then((result) => {
+            if (result.status) {
+                let data = result.data
+                this.setState({ items: data }, () => {
+                    // console.warn(this.state.items)
+                })
+            } else {
             }
+        })
+    }
+
+    componentDidMount() {
+        this.handleGetItem()
+   
+        // this.setUserData();
+    }
+
+    // setUserData = () => {
+    //     let user = { ...this.props.globalState.loginData };
+    //     this.setState({
+    //         id: user.user_id
+    //     })
+    // }
+  render(){
+
+    let item = {...this.state.item}
+        console.warn(item)
+        return (
+            <View style={{ width : '100%', paddingHorizontal : 14,paddingBottom : 28,alignContent:"center" }}>
+                <View style={{padding : 14}}>
+                    <Image source={{uri: `${Settings.basePath}${item.item_pic}`}}  resizeMode="cover" style={AppStyles.home.redeemPic}></Image>
+                </View>
+                <Text style={{textAlign:"center"}}>{item.item_name}</Text>
+                <Text style={{textAlign:"center"}}>{item.item_description}</Text>
+                <View style={styles.formGroup}>
+                <TouchableOpacity style={styles.btn}>
+                <View style={{ flex: 1,flexDirection: 'row',alignItems: 'center',justifyContent: "center",fontSize: 20}}>
+                    <Image source={require("../../assets/images/icons/icon_coin.png")}  style={{width : 10, height : 10}} />
+                    <Text style={{fontSize : 18,color:"white"}}>{item.item_point}</Text>
+                </View>
+                 </TouchableOpacity>
+             </View>
             </View>
         )
 
